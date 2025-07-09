ec3d041aa2f0d688716fcf6894850369f5e740d2
FrancisMurayaDev <francismurayadev@gmail.com>
2025-07-09 09:05:05 +0300

    update site

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
+  e.preventDefault();
 };
 
 const closeModal = function () {
@@ -18,8 +19,9 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(function (btn) {
+  btn.addEventListener(`click`, openModal);
+});
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
